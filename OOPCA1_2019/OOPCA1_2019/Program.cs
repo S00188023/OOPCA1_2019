13fb658cb2d3e3671ad7e021ba1e025b20845be6
S00188023 <S00188023@users.noreply.github.com>
2019-11-04 12:34:23 +0000

    CA1

---
+++ OOPCA1_2019/OOPCA1_2019/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPCA1_2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Song> titles = new List<Song>();
+            //List<Song> artist = new List<Song>();
+            //List<Song> duration = new List<Song>();
+            //List<Song> genre = new List<Song>();
+
+            Song MasterOfPuppets = new Song("Master of Puppets");
+            Song LoseYourself = new Song("Lose Yourself");
+            Song Hello = new Song("Hello");
+            Song PartyRockAnthem = new Song("Party Rock Anthem");
+            Song HighwayToHell = new Song("Highway to Hell");
+
+            titles.Add(MasterOfPuppets);
+            titles.Add(LoseYourself);
+            titles.Add(Hello);
+            titles.Add(PartyRockAnthem);
+            titles.Add(HighwayToHell);
+
+            
+
+            titles.Sort();
+            titles.Reverse();
+
+            PlayList(titles);
+
+
+            static void PlayList(List<Song> titles)
+            {
+                Console.WriteLine("\n{0,-15}{1,-7}{3,-7}{4,-7}{5,-7}", "NAME", "POINTS", "WINS", "DRAWS", "LOSSES", "PLAYED");
+
+                foreach (Song Title in titles)
+                {
+                    Console.WriteLine(titles.
+                }
+            }
+        }
+    }
+}
---
+++ OOPCA1_2019/OOPCA1_2019/Song.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPCA1_2019
+{
+    public enum Genre{ Rock, Pop, Dance, Other }
+
+    class Song
+    {
+        string title, artist;
+        double duration;
+        Genre genre;
+
+        
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public double Duration { get; set; }
+        public Genre Genre { get; set; }
+
+        public Song(string title/*, string artist, double duration, Genre genre*/)
+        {
+            Title = title;
+            Artist = artist;
+            Duration = duration;
+            Genre = genre;
+        }
+
+        public void AddGenre(Genre genre)
+        {
+
+        }
+
+
+        public override string ToString()
+        {
+            return string.Format($"{Title} - {Artist} - {Duration} - {Genre}");
+        }
+
+
+        
+
+        
+    }
+}
